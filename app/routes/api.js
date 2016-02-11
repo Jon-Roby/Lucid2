cda2e4fa36a102c28b5166f91fb93432da4ea70e
Jon Roby <jonathan.roby1@gmail.com>
2016-02-10 20:06:03 -0800

    Add config variables for cloudinary

--- app/routes/api.js
+++ app/routes/api.js
@@ -228,9 +228,9 @@ module.exports = function(app, express) {
 			User.findById(req.params.user_id)
 				.then(function(user) {
 					cloudinary.config({
-					  cloud_name: CLOUD_NAME,
-					  api_key: API_KEY,
-					  api_secret: API_SECRET
+					  cloud_name: process.env.CLOUD_NAME,
+					  api_key: process.env.API_KEY,
+					  api_secret: process.env.API_SECRET
 					});
 
 					cloudinary.uploader.upload(req.body.photo)
